c9272c76da1d48715e51d940bbcc7f7e0aec5033
Uday Yadav <yadav117uday@gmail.com>
2022-02-22 18:00:18 +0530

    init collection service

---
+++ collection-service/src/main/java/com/backend/collectionservice/CollectionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.backend.collectionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionServiceApplication.class, args);
+	}
+
+}
