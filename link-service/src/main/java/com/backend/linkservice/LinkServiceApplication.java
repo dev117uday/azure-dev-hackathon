7e0aebd743566e007f5f6a01dbbce1577381ff51
Uday Yadav <yadav117uday@gmail.com>
2022-02-22 18:01:55 +0530

    init link service

---
+++ link-service/src/main/java/com/backend/linkservice/LinkServiceApplication.java
@@ -0,0 +1,13 @@
+package com.backend.linkservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkServiceApplication.class, args);
+	}
+
+}
---
+++ link-service/src/test/java/com/backend/linkservice/LinkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.linkservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
